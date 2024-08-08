cf6714cd10c0650e1fbf7165ed709ca0917315db
michelynr <michelynr28@gmail.com>
2024-08-08 17:35:21 +0530

    revisi index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,49 +1,50 @@
+// src/types/index.ts
+
 export interface User {
-    userId: number;
-    username: string;
-    email: string;
-    password: string;
-    location: string;
-    userType: 'CUSTOMER' | 'SELLER';
-  }
-  
-  export interface Product {
-    productId: number;
-    sellerId: number;
-    productName: string;
-    description: string;
-    price: number;
-    quantity: number;
-    category: string;
-    ecoFriendly: boolean;
-  }
-  
-  export interface CartItem {
-    cartItemId: number;
-    cartId: number;
-    productId: number;
-    quantity: number;
-    price: number;
-  }
-  
-  export interface Cart {
-    cartId: number;
-    customerId: number;
-    cartItems: CartItem[];
-    totalPrice: number;
-  }
-  
-  export interface Order {
-    orderId: number;
-    customerId: number;
-    totalPrice: number;
-    orderDate: string;
-  }
-  
-  export interface Voucher {
-    voucherId: number;
-    sellerId: number;
-    voucherCode: string;
-    discountPercentage: number;
-  }
-  
+  userId: number;
+  username: string;
+  email: string;
+  password: string;
+  location: string;
+  userType: 'CUSTOMER' | 'SELLER';
+}
+
+export interface Product {
+  productId: number;
+  sellerId: number;
+  productName: string;
+  description: string;
+  price: number;
+  quantity: number;
+  category: string;
+  ecoFriendly: boolean; 
+}
+
+export interface CartItem {
+  cartItemId: number;
+  cartId: number;
+  productId: number;
+  quantity: number;
+  price: number;
+}
+
+export interface Cart {
+  cartId: number;
+  customerId: number;
+  cartItems: CartItem[];
+  totalPrice: number;
+}
+
+export interface Order {
+  orderId: number;
+  customerId: number;
+  totalPrice: number;
+  orderDate: string; // Format date-time
+}
+
+export interface Voucher {
+  voucherId: number;
+  sellerId: number;
+  productId: number;
+  discount: number; // Persentase diskon
+}
