acabe773f569873c28456c8d58947ced480262c4
fawwazaid <fawwazaid@gmail.com>
2024-08-13 19:19:23 +0700

    edit components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import HeroBanner from "@/components/herobanner";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>Hello</div>
+      <HeroBanner />
     </main>
   );
 }
