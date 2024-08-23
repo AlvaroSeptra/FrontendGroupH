9c7c90a543786feaaf1b5573b258cc2239bda21b
fawwazaid <fawwazaid@gmail.com>
2024-08-23 07:40:26 +0700

    edit layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-<<<<<<< Updated upstream
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
-=======
+
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/footer";
->>>>>>> Stashed changes
+
 import { CartProvider } from "@/components/CartContext";
 
 const inter = Inter({ subsets: ["latin"] });
